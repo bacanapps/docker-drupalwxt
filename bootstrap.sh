fa849a59c6afce4ab287044dedc4dde827b3790c
William Hearn <sylus1984@gmail.com>
2014-12-01 02:10:45 -0500

    Fix for force yes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ if [ ! -f app/sites/default/settings.php ]; then
 
   # Install Drupal WxT
   cd app
-  drush si wetkit wetkit_theme_selection_form.theme=wetkit_bootstrap install_configure_form.demo_content=TRUE --db-url=pgsql://super:wetkit2015@10.0.0.200:5432/drupalwxt --sites-subdir=default --account-name=admin --account-pass=wetkit2015 --site-name="WxT" —-yes
+  drush si wetkit wetkit_theme_selection_form.theme=wetkit_bootstrap install_configure_form.demo_content=TRUE --root=/var/www/html --db-url=pgsql://super:wetkit2015@10.0.0.200:5432/drupalwxt --sites-subdir=default --account-name=admin --account-pass=wetkit2015 --yes
 
   sleep 5s
 fi
